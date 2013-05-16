7af563f77482e7c6a9be30f581d7ce086e3b75a5
Brandon Keene <bkeene@gmail.com>
2013-05-16 15:48:20 -0400

    post-header-key-sort

--- test/signature_test.rb
+++ test/signature_test.rb
@@ -65,4 +65,18 @@ describe AWS4::Signature do
     signed["Host"].must_equal("host.foo.com")    
     signed["Authorization"].must_equal("AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=date;host, Signature=22902d79e148b64e7571c3565769328423fe276eae4b26f83afceda9e767f726")    
   end
+
+  it "signs post-header-key-sort" do
+    uri = URI("http://host.foo.com/")
+    headers = {
+      "Host" => "host.foo.com",
+      "Date" => "Mon, 09 Sep 2011 23:36:00 GMT",
+      "ZOO"  => "zoobar"
+    }
+    body = ""
+    signed = signature.sign("POST", uri, headers, body)
+    signed["Date"].must_equal("Mon, 09 Sep 2011 23:36:00 GMT")
+    signed["Host"].must_equal("host.foo.com")    
+    signed["Authorization"].must_equal("AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=date;host;zoo, Signature=b7a95a52518abbca0964a999a880429ab734f35ebbf1235bd79a5de87756dc4a")        
+  end
 end
