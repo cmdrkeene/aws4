eb7e3bcfbfe33137057bff62b2032ed5764eea85
Brandon Keene <bkeene@gmail.com>
2013-05-16 16:28:50 -0400

    post-vanilla-empty-query-value

--- test/signature_test.rb
+++ test/signature_test.rb
@@ -91,6 +91,19 @@ describe AWS4::Signature do
     signed["Authorization"].must_equal("AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=date;host, Signature=22902d79e148b64e7571c3565769328423fe276eae4b26f83afceda9e767f726")    
   end
 
+  it "signs post-vanilla-empty-query-value" do
+    uri = URI("http://host.foo.com/?foo=bar")
+    headers = {
+      "Host" => "host.foo.com",
+      "Date" => "Mon, 09 Sep 2011 23:36:00 GMT"
+    }
+    body = ""
+    signed = signature.sign("POST", uri, headers, body)
+    signed["Date"].must_equal("Mon, 09 Sep 2011 23:36:00 GMT")
+    signed["Host"].must_equal("host.foo.com")    
+    signed["Authorization"].must_equal("AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=date;host, Signature=b6e3b79003ce0743a491606ba1035a804593b0efb1e20a11cba83f8c25a57a92")    
+  end
+
   it "signs post-header-key-sort" do
     uri = URI("http://host.foo.com/")
     headers = {
